798a481918425d3cd4148053f9e8b58983b49bf4
Akash Pal <73605611+akashcodepal@users.noreply.github.com>
2021-07-02 12:43:18 +0530

    Second Commit
    Updation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,10 +7,10 @@ export default function Navbar(props) {
     return (
         <div>
             <div className="row container-fluid pt-4 mx-4">
-                <h3 className="text-success  col-6 pl-4"><u>Weather App</u></h3>
+                <h3 className="text-black  col-6 pl-4"><u>Weather App</u></h3>
                 <form className=" row col-6 justify-content-end text-white">
                    <div className="col-4 md-2">
-                     <input type="text" required className="form-control bg-transparent border-top-0 border-left-0 border-right-0 rounded-0 border-white text-white "placeholder="City" onChange={(e)=>handleCity(e.target.value)}/>
+                     <input type="text" required className="form-control bg-black border-top-0 border-left-0 border-right-0 rounded-0 border-secondary text-black "placeholder="Enter City" onChange={(e)=>handleCity(e.target.value)}/>
                    </div>
                    <button className="btn btn-success md-2" onClick={(e)=>getWeather(e)}>Get Weather</button>
                 </form>     
